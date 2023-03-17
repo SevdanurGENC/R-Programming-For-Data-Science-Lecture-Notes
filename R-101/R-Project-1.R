f2ac47fc0093a7e53e0b901f3c9d3bf770f731ec
Dr. Nano / Sevdanur GENC <sevdanurgenc@gmail.com>
2023-03-18 00:16:23 +0300

    Add files via upload

---
+++ R-101/R-Project-1.R
@@ -0,0 +1,69 @@
+# R programlama için örnek kodlar
+
+# ekrana bir şey yazdırma  
+print("Merhaba R")
+
+# Değişkene değer atama
+a = 3
+3 -> a
+a <- 3
+a
+
+# her üç durumda da a'nın değeri 3 olarak belirleniyor.
+yazi = "Merhaba R" # string ifadeler "" içinde belirtilir.
+yazi
+
+# Matematiksel operatörler
+b = 50
+c = 90
+
+b/c
+b+c
+b-c
+b*c
+
+d = 5
+e = 2
+
+d^e #üs alma şlemi
+
+#seri oluşturma
+seri = seq(30:10)
+seri2 = c(10:20) #10'dan 20'ye kadar sayılar serisi
+seri2
+seri3 = c(20:10) #20'den 10 a kadar sayılar serisi
+seri3
+seri4 = c(10,20) #sadece 10 ve 20 den oluşan seri
+seri4
+
+#seri işlemleri
+seri
+seri[6] #serinin 6. elemanı
+seri3
+seri3[c(2,3,4)] #seri 3 ün 2,3, ve 4. elemanları sırasıyla 
+seri3[2:4] #hemen üstteki ile aynı işi yapıyor
+seri2[seri2<16 & seri2 >12] #seri2 nin 16 dan küçük ve 12'den büyük elemanları
+seri[seri>15 | seri <15] #serinin 15 ten büyük veya 15 ten küçük elemanları (hepsi)
+# & işareti ve anlamına gelir. | işareti ise veya anlamına gelir.
+# bu işaretler yukarıdaki gibi işlemlerde kullanılabilir.
+
+#seri elemanlarını birleştirme
+paste(c("x","y"), 0:10, sep = ",")
+paste(c(3,5), 0:10, sep = "*")
+paste(c(10), 0:5, sep = "*")
+paste(seri3, seri2, sep = "-") #seri3 ve seri2 elemanlarını "-" işaretiyle birleştirecek
+
+
+#Matematiksel fonksiyonalar
+log10(10) #log10 tabanında 10
+log(3, 9) #logaritma 9 tabanında 3
+tan(45)
+
+# Fonksiyon oluşturma
+g = function(x,y) 
+{
+(x^2) - (2*y) + 6
+}
+
+g(10,20)
+
---
+++ R-101/R-Project-4.R
@@ -0,0 +1,19 @@
+# R Project Ortamın Kaydedilmesi ve Scriptler
+
+x = c(1,2,3,4,5,6) # liste oluşturma
+x
+x = seq(1:6) # 1'den 6'ya kadar olan sayılar için bu şekilde de oluşturabiliriz.
+getwd() # şu anki çalışma dizinini gösterir
+
+setwd("C:/Users/Nano/Desktop") # çalışma dizinini yeniden istediğimiz bir yere belirtebiliriz.
+
+save.image("kayit.rdata") #şu anki çalışma içeriğimizi bir dosyaya kaydettik.
+
+load("kayit.rdata") # kaydettiğimiz dosyayı çalışma ortamına yükeldik.
+
+load(file.choose()) #burada açılan pencereden de yüklemek istediğimiz dosyayı seçebiliriz.
+# yukarıdaki konutu program yazarken veriyi yüklemek için de kullanabiliriz.
+ls # dosyayı yükledikten sonra oluşturduğumuz x burada görünecek.
+#denemek için programı açıp kapatın ve değişkenlerin yüklendiğini göreceksiniz.
+
+#remove(list = ls()) # oluşturduğumuz bütün değişkenleri hafızadan sildik
